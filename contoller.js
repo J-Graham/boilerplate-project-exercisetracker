91b0399b9bcdda6cb29948ad90b0f92892754850
unknown <johngrhm@gmail.com>
2019-02-09 09:54:44 -0500

    finished post end points

---
+++ contoller.js
@@ -0,0 +1,4 @@
+var app = express();
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
 const cors = require('cors')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+require('dotenv').config()
+const mongoose = require('mongoose');
+console.log(process.env.MLAB_URI);
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
@@ -17,11 +17,73 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+// *************
+// MODELS
+// *************
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  name: { type: String, unique: true }
+});
+var User = mongoose.model('User', userSchema);
 
+var exerciseSchema = new Schema({
+  userId: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now() }
+});
+var Exercise = mongoose.model('Exercise', exerciseSchema);
+
+// *************
+// Endpoints
+// *************
+app.post('/api/exercise/new-user', (req, res) => {
+  if (!req.body.username || req.body.username === '') {
+    res.send(`Path 'username' is required`);
+  } else {
+
+    var user = new User({ name: req.body.username });
+    user.save((err, data) => {
+      if (err) {
+        res.send(`${req.body.username} already exists`);
+      } else {
+        res.json({ username: req.body.username, _id: data._id });
+      }
+    });
+  }
+});
+
+app.post('/api/exercise/add', (req, res) => {
+  if (!req.body.userId || req.body.userId === '') {
+    res.send(`'UserId' is required`);
+  } else if (!req.body.description || req.body.description === '') {
+    res.send(`'Description' is required`);
+  } else if (!req.body.duration || req.body.duration === '') {
+    res.send(`'duration' is required`);
+  } else {
+    User.findById(req.body.userId, (err, data) => {
+      if (err && err.message.indexOf('Cast to ObjectId failed') !== -1) {
+        res.send("No user found");
+      } else {
+        console.log('here');
+        var exercise = new Exercise({ userId: req.body.userId, description: req.body.description, duration: req.body.duration, date: req.body.date ? req.body.date : Date.now() });
+        console.log(exercise);
+        exercise.save((err, data) => {
+          if (err) {
+            res.send(err.message);
+          } else {
+            console.log(data);
+            res.json(data);
+          }
+        });
+      }
+    });
+  }
+});
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+  return next({ status: 404, message: 'not found' })
 })
 
 // Error Handling middleware
@@ -42,6 +104,9 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
